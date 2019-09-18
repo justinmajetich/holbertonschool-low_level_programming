c438832c37bff841d6600068452063f505620788
justinmajetich <justinmajetich@gmail.com>
2019-09-18 23:50:57 +0000

    Create script for adv task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* Main - Print to standard error
+*
+* Return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "\"and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
