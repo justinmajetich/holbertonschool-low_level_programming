f414d88cdcb4a361adb558cb630e31f9461cd0fa
justinmajetich <justinmajetich@gmail.com>
2019-09-19 15:57:51 +0000

    Create script to print single digit nums in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print single digits of base 10
+*
+* Return: 0
+*/
+int main(void)
+	{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+		{
+		printf("%d", i);
+		i++;
+		}
+
+	printf("\n");
+
+	return (0);
+	}
