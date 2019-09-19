c6a42a15ebbe9ab9fc0d5556d948d057613c0bb2
justinmajetich <justinmajetich@gmail.com>
2019-09-19 16:02:31 +0000

    Fix typo in filename

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print lower alpha except "q" and "e"
+*
+* Return: 0
+*/
+int main(void)
+	{
+	char c;
+
+	c = 'a';
+
+	while (c < '{')
+		{
+		if (c != 'q' && c != 'e')
+			{
+			putchar(c);
+			}
+		c++;
+		}
+
+		putchar('\n');
+
+	return (0);
+	}
