18472caca24e58b3b326ba45020cf78dea875c32
justinmajetich <justinmajetich@gmail.com>
2019-09-19 19:59:44 +0000

    Create script to list number 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 00 to 99
+ *
+ * Return: (0)
+ */
+int main(void)
+	{
+	int i;
+	int j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+		{
+		while (j < 58)
+			{
+			putchar(i);
+
+			putchar(j);
+
+				{
+				if (i != 57 && j != 57)
+				putchar(',');
+				putchar(' ');
+				}
+			j++;
+			}
+		i++;
+		j = 48;
+		}
+	putchar('\n');
+
+	return (0);
+	}
