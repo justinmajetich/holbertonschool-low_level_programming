ccdc35f3d9d49179a49d2e85bb6b1d3f1f102218
justinmajetich <justinmajetich@gmail.com>
2019-09-19 00:15:04 +0000

    Correct Main to main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* Main - Print to standard error
+* main - Print to standard error
 *
 * Return: 1
 */
 int main(void)
 {
-	fprintf( stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (1);
+return (1);
 }
