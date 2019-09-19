90648636301b99e4c5b5792c9d7186418b1a94bc
justinmajetich <justinmajetich@gmail.com>
2019-09-19 14:56:21 +0000

    Create script to print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print lowercase alphabet
+*
+* Return: 0
+*/
+int main(void)
+	{
+	char c;
+
+	c = 'a';
+
+	while (c < 123)
+		{
+		putchar(c);
+		c++;
+		}
+
+		putchar('\n');
+
+	return (0);
+	}
