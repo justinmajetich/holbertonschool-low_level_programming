9a701179e96e6fb32d6d75dbcf8096bf45e93187
justinmajetich <justinmajetich@gmail.com>
2019-09-23 20:24:57 +0000

    Create script to print lower alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ *
+ * Description: Alphabet is printed with a for loop. Process
+ * is repeated ten times with while loop.
+ * Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{	
+			_putchar(c);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
