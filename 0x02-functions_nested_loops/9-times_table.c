fc040b4498f51591824ebd73346f772b0cc187c9
justinmajetich <justinmajetich@gmail.com>
2019-09-24 13:44:08 +0000

    Edit indentation

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,27 +15,26 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			if (j != 0)
+			{
+				_putchar(' ');
+			}
+
+			if ((i * j) / 10 != 0)
+			{
+				_putchar(((i * j) / 10) + '0');
+			}
+			else
+			{
 				if (j != 0)
-				{
 					_putchar(' ');
-				}
+			}
+			_putchar(((i * j) % 10) + '0');
 
-				if ((i * j) / 10 != 0)
-				{
-					_putchar(((i * j) / 10) + '0');
-				}
-				else
-				{
-					if (j != 0)
-						_putchar(' ');
-				}
-
-				_putchar(((i * j) % 10) + '0');
-
-				if (j != 9)
-				{
-					_putchar(',');
-				}
+			if (j != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
