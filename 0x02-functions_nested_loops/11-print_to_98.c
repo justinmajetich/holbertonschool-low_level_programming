a522ae6fe4318261a701ff091603172be9fb1a18
justinmajetich <justinmajetich@gmail.com>
2019-09-24 16:54:02 +0000

    Remove whitespace

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
  */
 void print_to_98(int n)
 {
-	
+
 	if (n <= 98)
 		for (; n <= 98; n++)
 		{
@@ -23,10 +23,9 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			printf("%i", n);
-			
+
 			if (n != 98)
 				printf(", ");
-
 		}
 		printf("\n");
 }
