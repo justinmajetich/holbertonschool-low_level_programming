e5c99058035ba1d3d429e14366c22b56d06eb6ac
justinmajetich <justinmajetich@gmail.com>
2019-09-24 14:31:11 +0000

    Add variable

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,18 +10,18 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
-		n *= -1;
-		n %= 10;
-		_putchar(n + '0');
-		return (n);
+		last digit = -n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
-
 	else
 	{
-		n %= 10;
-		_putchar(n + '0');
-		return (n);
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
 }
