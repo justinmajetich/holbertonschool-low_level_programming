3a78b687f5febc57a3e3ffeb91fdbe2da019ef73
justinmajetich <justinmajetich@gmail.com>
2019-09-25 01:34:50 +0000

    Write script to print first 50 Fib #s

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 Fibonacci
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i, num1 = 0, num2 = 1, numx;
+
+	for (i = 0; i <= 49; i++)
+	{
+		printf("%d", num1);
+
+		numx = num1 + num2;
+		num1 = num2;
+		num2 = numx;
+
+		if (i != 49)
+			printf(", ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
