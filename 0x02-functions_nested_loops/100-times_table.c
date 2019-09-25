1eb2a75a546822fd8d47b39bf7f7371c4ccba6a0
justinmajetich <justinmajetich@gmail.com>
2019-09-25 00:46:05 +0000

    Create script to print times table of n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints times table of n
+ * @n: number to create table for
+ *
+ * Description: Using nested loops,
+ * prints computes and prints n times table.
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j != 0)
+					_putchar(' ');
+
+				if ((i * j) / 100 != 0)
+					_putchar(((i * j) / 100) + '0');
+				else
+				{
+					if (j != 0)
+						_putchar(' ');
+				}
+
+				if ((i * j) / 10 != 0)
+					_putchar((((i * j) / 10) % 10) + '0');
+				else
+				{
+					if (j != 0)
+						_putchar(' ');
+				}
+				_putchar(((i * j) % 10) + '0');
+
+				if (j != n)
+					_putchar(',');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
