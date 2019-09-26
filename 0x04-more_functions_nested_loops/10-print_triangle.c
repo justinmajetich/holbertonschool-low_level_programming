a1ec138e3e3825ee72bb5df69c25f7dcc04e6f75
justinmajetich <justinmajetich@gmail.com>
2019-09-26 17:42:28 +0000

    Add switch for > 0

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,18 @@ void print_triangle(int size)
 	int j;
 	int space_cnt;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (space_cnt = size - i; space_cnt > 0; space_cnt--)
-			_putchar(' ');
-
-		for (j = 0; j < i; j++)
+	if (size > 0)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (space_cnt = size - i; space_cnt > 0; space_cnt--)
+				_putchar(' ');
+
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	else
 		_putchar('\n');
-	}
 }
