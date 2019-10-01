727561629b9e76872912c7e55b568928b6f5dfb4
justinmajetich <justinmajetich@gmail.com>
2019-10-01 13:38:12 +0000

    Replace s[i] with i in condition

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void print_rev(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		length++;
 
-	for (i = length - 1; s[i] >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
