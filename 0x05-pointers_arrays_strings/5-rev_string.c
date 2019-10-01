046476b53c35c3d15369f657993f97381772edeb
justinmajetich <justinmajetich@gmail.com>
2019-10-01 16:13:51 +0000

    Use temp char vars

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i, j = 0, length = 0;
+	char *rev = s;
+	char c0 = 0, c1 = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+	for (i = length - 1; i > (i / 2);)
+	{
+		c0 = rev[j];
+		c1 = s[i];
+		rev[j] = c1;
+		s[i] = c0;
+
+		j++;
+		i--;
+	}
+
+	for (j = 0, i = 0; rev[j] != '\0'; j++, i++)
+		s[i] = rev[j];
+}
