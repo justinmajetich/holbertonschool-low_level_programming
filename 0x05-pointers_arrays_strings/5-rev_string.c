353679c9d8b05377e91180db4eca8a4343856bf1
justinmajetich <justinmajetich@gmail.com>
2019-10-01 16:38:56 +0000

    Add conditional to handle odd/even

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,19 @@
  */
 void rev_string(char *s)
 {
-	int i, j = 0, length = 0;
+	int i, j = 0, length = 0, half = 0;
 	char *rev = s;
 	char c0 = 0, c1 = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		length++;
 
-	for (i = length - 1; i > (length / 2);)
+	if (length % 2 == 0)
+		half = length / 2;
+	else
+		half = (length - 1) / 2;
+
+	for (i = length - 1; i >= half;)
 	{
 		c0 = rev[j];
 		c1 = s[i];
