14b4e901566f49ca70975231517bc77fd547c53f
justinmajetich <justinmajetich@gmail.com>
2019-10-02 02:09:25 +0000

    Filters for overflow

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <limits.h>
 
 /**
  * _atoi - converts string to integer
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, digit, integer = 0, num_switch = 0;
+	int i = 0, sign = 1, digit, intg = 0, num_switch = 0;
 
 	for (; s[i] != '\0'; i++)
 	{
@@ -18,18 +18,24 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = (s[i] - '0');
-			integer = (integer * 10) + digit;
+
+			if (intg > INT_MAX / 10 || intg * 10 > INT_MAX - digit)
+				return (INT_MAX);
+			else if (intg < 0 && (intg < INT_MIN / 10 || intg * 10 < INT_MIN - digit))
+				return (INT_MIN);
+			else
+				intg = (intg * 10) + digit;
+
+			if (sign == -1 && intg >= 0)
+				intg *= -1;
+
 			num_switch = 1;
 		}
 		if ((s[i] < '0' || s[i] > '9') && num_switch == 1)
 			break;
 	}
-
-	if (sign < 0)
-		integer *= -1;
-
-	if (integer == 0)
+	if (intg == 0)
 		return (0);
 	else
-		return (integer);
+		return (intg);
 }
