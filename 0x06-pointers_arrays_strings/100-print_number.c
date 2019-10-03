51435e8ced05dcff4435ecf1e4b08c99df1a4920
justinmajetich <justinmajetich@gmail.com>
2019-10-03 21:44:42 +0000

    Create script to print integer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * print_number - print integer
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	int zero_switch = 0;
+
+	(n < 0) ? _putchar('-') : (n *= -1);
+	if ((n / 1000000000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 1000000000) % 10 + '0');
+	if ((n / 100000000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 100000000) % 10 + '0');
+	if ((n / 10000000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 10000000) % 10 + '0');
+	if ((n / 1000000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 1000000) % 10 + '0');
+	if ((n / 100000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 100000) % 10 + '0');
+	if ((n / 10000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 10000) % 10 + '0');
+	if ((n / 1000) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 1000) % 10 + '0');
+	if ((n / 100) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 100) % 10 + '0');
+	if ((n / 10) % 10 != 0)
+		zero_switch = 1;
+	if (zero_switch == 1)
+		_putchar(-(n / 10) % 10 + '0');
+	_putchar((n % 10) * -1 + '0');
+}
