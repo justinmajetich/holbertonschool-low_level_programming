b4ea83112cc68886ad334a3a3ae0f3852713fac0
justinmajetich <justinmajetich@gmail.com>
2019-10-03 19:29:06 +0000

    Exchange hard returns values for flexible

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,12 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != 0)
 	{
 		if (s1[i] < s2[i])
-			return (-15);
+			return (s1[i] - s2[i]);
 		if (s1[i] > s2[i])
-			return (15);
+			return (s1[i] - s2[i]);
 		i++;
 	}
+	if (s2[i] != 0)
+		return (s1[i] - s2[i]);
 	return (0);
 }
