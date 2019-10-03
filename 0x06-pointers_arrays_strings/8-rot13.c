390201b1331e58176d54d131218976dd08ece51e
justinmajetich <justinmajetich@gmail.com>
2019-10-03 21:09:56 +0000

    Break loop after encryption occurs

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,25 +9,19 @@
 char *rot13(char *s)
 {
 	int str_cnt = 0, al_cnt;
-	char low_al[53] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-	char up_al[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot_alpha[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (s[str_cnt] != 0)
 	{
 		for (al_cnt = 0; al_cnt < 52;)
 		{
-			if (s[str_cnt] == low_al[al_cnt])
+			if (s[str_cnt] == alpha[al_cnt])	
 			{
-				s[str_cnt] = low_al[al_cnt + 13];
+				s[str_cnt] = rot_alpha[al_cnt];
 				break;
 			}
-			else if (s[str_cnt] == up_al[al_cnt])
-			{
-				s[str_cnt] = up_al[al_cnt + 13];
-				break;
-			}
-			else
-				al_cnt++;
+			al_cnt++;
 		}
 		str_cnt++;
 	}
