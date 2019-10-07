4d0eb07a9c633554e942505b7d649815a3d57217
justinmajetich <justinmajetich@gmail.com>
2019-10-07 01:37:01 +0000

    Add if condition to test if size > 0

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -10,41 +10,42 @@ void print_buffer(char *b, int size)
 {
 	int thru_cnt, col_cnt, spc_cnt;
 
-	thru_cnt = 0;
-	while (thru_cnt <= size - 1)
+	if (size > 0)
 	{
-		if (thru_cnt % 10 == 0)
-			printf("%08x: ", thru_cnt);
-
-		printf("%02x", b[thru_cnt]);
-
-		if (thru_cnt == size - 1)
-			for (spc_cnt = size % 10; spc_cnt <= 9; spc_cnt++)
-				printf("  ");
-
-		if (thru_cnt != 0 && thru_cnt % 2 != 0)
-			printf(" ");
-		if (thru_cnt == size - 1)
-			for (spc_cnt = (size % 10) / 2; spc_cnt <= 4; spc_cnt++)
-				printf(" ");
-
-		if ((thru_cnt + 1) % 10 == 0 || thru_cnt == size - 1)
+		for (thru_cnt = 0; thru_cnt <= size - 1; thru_cnt++)
 		{
+			if (thru_cnt % 10 == 0)
+				printf("%08x: ", thru_cnt);
+			printf("%02x", b[thru_cnt]);
 			if (thru_cnt == size - 1)
-				col_cnt = thru_cnt - ((size - 1) % 10);
-			else
-				col_cnt = thru_cnt - 9;
+				for (spc_cnt = size % 10; spc_cnt <= 9; spc_cnt++)
+					printf("  ");
 
-			while (col_cnt <= thru_cnt)
+			if (thru_cnt != 0 && thru_cnt % 2 != 0)
+				printf(" ");
+			if (thru_cnt == size - 1)
+				for (spc_cnt = (size % 10) / 2; spc_cnt <= 4; spc_cnt++)
+					printf(" ");
+
+			if ((thru_cnt + 1) % 10 == 0 || thru_cnt == size - 1)
 			{
-				if (b[col_cnt] >= 32 && b[col_cnt] <= 126)
-					printf("%c", b[col_cnt]);
+				if (thru_cnt == size - 1)
+					col_cnt = thru_cnt - ((size - 1) % 10);
 				else
-					printf(".");
-				col_cnt++;
+					col_cnt = thru_cnt - 9;
+
+				while (col_cnt <= thru_cnt)
+				{
+					if (b[col_cnt] >= 32 && b[col_cnt] <= 126)
+						printf("%c", b[col_cnt]);
+					else
+						printf(".");
+					col_cnt++;
+				}
+				printf("\n");
 			}
-			printf("\n");
 		}
-		thru_cnt++;
 	}
+	else
+		printf("\n");
 }
