369bf15bf452b7c092d89427f5f80fb969e163a3
justinmajetich <justinmajetich@gmail.com>
2019-10-09 17:22:30 +0000

    Write script to times by exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - raise x to power of y
+ * @x: number to change
+ * @y: exponent
+ *
+ * Return: x to the yth power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
