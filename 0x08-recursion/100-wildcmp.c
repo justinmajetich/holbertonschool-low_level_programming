092f8c5d23377a9e1e6d861d82065b0974d77b80
justinmajetich <justinmajetich@gmail.com>
2019-10-10 21:19:22 +0000

    Write script to compare strings including wildcards

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int subcmp(char *s1, char *s2, int i);
+
+/**
+ * wildcmp - compare two strings
+ * @s1: string to be compared
+ * @s2: string to compare to
+ *
+ * Return: 1 if identical, 0 if differing
+ */
+int wildcmp(char *s1, char *s2)
+{
+	/* if s1 and s2 are both at '\0' return 1 */
+	if (!*s1 && !*s2)
+		return (1);
+	/* if s1 is equal to s2 proceed thru both */	
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	/* if s1 and s2 are not equal */	
+	else if (*s1 != *s2)
+	{
+		/* if s1 is not s2 but s2 is '*' */
+		if (*s2 == '*')
+		{
+			/* if s2 is followed by another '*' proceed thru s2 */
+			if (*(s2 + 1) == '*')
+				return (wildcmp(s1, s2 + 1));
+			else if (*(s2 + 1) == '\0' && *s1 == '\0')
+				return (1);
+			/* if s2 is not followed by '*' proceed from next s1 that matches s2 + 1 */
+			else
+			{
+				return (wildcmp(s1 + subcmp(s1, s2, 0), s2 + 1));
+			}
+		}
+		return (0);
+	}
+	return (1);
+}
+
+int subcmp(char *s1,char *s2, int i)
+{
+	if (*(s1 + i) == *(s2 + 1))
+		return (i);
+	else if (*(s1 + i) == '\0')
+		return (0);
+	else
+		return subcmp(s1, s2, i + 1);	
+}
