caec1070a7f2a4c97f4a03a6c5989c4c8ebe06cc
justinmajetich <justinmajetich@gmail.com>
2019-10-13 18:43:14 +0000

    Create script to multiply numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int convert_string(char **string);
+int convert_string(char *string);
 
 /**
  * main - multiply two numbers
@@ -12,31 +12,47 @@ int convert_string(char **string);
  */
 int main(int argc, char **argv)
 {
-	int num1 = convert_string(argv[1]);
-	int nums = convert_string(argv[2]);
+	int num1 = 0, num2 = 0;
 
-	if (argc == 2)
+	if (argc != 3)
 	{
-		printf("%d\n", num1 * num2);
-		return (0);
-	}
-	else
-	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
+
+	num1 = convert_string(*(argv + 1));
+	num2 = convert_string(*(argv + 2));
+
+	printf("%d\n", num1 * num2);
+	return (0);
 }
 
+/**
+ * convert_string - convert string to int
+ * @string: string to convert
+ *
+ * Return: integer result on Success
+ */
 int convert_string(char *string)
 {
-	int digit = 0, result = 0, cnt = 0;
+	int digit = 0, result = 0, cnt = 0, sign = 1;
 
-	while (string[cnt--])
+	/* track sign */
+	if (string[cnt] == '-')
+	{
+		sign = -1;
+		cnt++;
+	}
+
+	while (string[cnt] != '\0')
 	{
 		/* convert ascii val to int */
 		digit = string[cnt] - '0';
 		/* times result up a place and add new digit */
 		result = (result * 10) + digit;
+		cnt++;
 	}
+	result *= sign;
+
 	return (result);
 }
