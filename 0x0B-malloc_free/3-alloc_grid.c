0532a44d653a77cc1177f9343c9c5fa0f706d6bd
justinmajetich <justinmajetich@gmail.com>
2019-10-15 22:12:38 +0000

    Free grid upon *grid malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,10 @@ int **alloc_grid(int width, int height)
 		if (*(grid + i) == NULL)
 		{
 			for (j = 0; j < i; j++)
+			{
+				free(grid);
 				free(*(grid + j));
+			}
 			return (NULL);
 		}
 	}
