f5e245d0ea1dd4cf4d73ecd554c676433d59977f
justinmajetich <justinmajetich@gmail.com>
2019-10-15 18:17:45 +0000

    Add guards for NULL arguments

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * str_concat - concatenate two strings
  * @s1: first string of concat
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len = 0;
 
 	/* take length of strings, accounting for null-byte */
-	for (i = 0; s1[i] != 0; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		len++;
-	for (i = 0; s2[i] != 0; i++)
+	for (i = 0; s2 != NULL && s2[i] != '\0'; i++)
 		len++;
 	len++;
 
@@ -27,9 +27,9 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	/* copy s1 and s2 successively to sCat, end with '\0' */
-	for (i = 0; s1[i] != 0; i++, j++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++, j++)
 		sCat[j] = s1[i];
-	for (i = 0; s2[i] != 0; i++, j++)
+	for (i = 0; s2 != NULL && s2[i] != '\0'; i++, j++)
 		sCat[j] = s2[i];
 	sCat[j] = '\0';
 
