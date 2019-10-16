a25036b8adee39ebdf8da32d029502872a8173fb
justinmajetich <justinmajetich@gmail.com>
2019-10-16 21:10:12 +0000

    Add if between 3rd and 4th loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,35 +12,32 @@ int main(void)
 	int k;
 	int l;
 
-	int l_add = 0;
-
 	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; j < 58;)
+		for (j = 48; j < 58; j++)
 		{
-			l_add = (j - 48) + 1;
-
 			for (k = i; k < 58; k++)
 			{
-				for (l = 48 + l_add; l < 58; l++)
-				{
-					if (!(i == k && j == l))
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(l);
+				if (k > i)
+					l = 48;
+				else
+					l = j + 1;
 
-						if (!(i == 57 && j == 56 && k == 57 && l == 57))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+				for (; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
-			j++;
 		}
 	}
 	putchar('\n');
