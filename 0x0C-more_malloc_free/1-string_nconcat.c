498cb91334ebc58cc872cdc5b27148107f7c043b
justinmajetich <justinmajetich@gmail.com>
2019-10-17 17:42:20 +0000

    Initialize len var

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cat;
-	unsigned int len, cnt, cat_cnt = 0;
+	unsigned int len = 0, cnt, cat_cnt = 0;
 
 	/* get s1 & s2 length */
 	if (s1)
