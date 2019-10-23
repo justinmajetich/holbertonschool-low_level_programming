a9922db5154ba62db4759b838e478aee8212408b
justinmajetich <justinmajetich@gmail.com>
2019-10-23 18:05:10 +0000

    Adjust incrementation style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,6 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	while (size--)
-		action(*array++);
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
 }
