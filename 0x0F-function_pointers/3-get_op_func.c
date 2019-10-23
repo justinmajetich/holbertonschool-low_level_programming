1feed77a7394071f6f0834fdb7257589c04f7027
justinmajetich <justinmajetich@gmail.com>
2019-10-23 18:45:42 +0000

    Swap out for loop for while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,12 +17,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
 		if (*ops[i].op == *s)
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
