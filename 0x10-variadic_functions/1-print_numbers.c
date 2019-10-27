dc97eb31255197e2743b246c27cd5f4930b72389
justinmajetich <justinmajetich@gmail.com>
2019-10-27 16:01:47 +0000

    Change position of separator NULL check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,17 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* initialize arg list */
 	va_start(args, n);
 
-	/* check is separator is NULL */
-	if (separator)
-		/* print all args */
-		while (i < n)
-		{
-			printf("%d", va_arg(args, int));
-				/* print separator if not last arg */
-				if (i + 1 != n)
-					printf("%s", separator);
-			i++;
-		}
+	/* print all args */
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+			/* print separator if not null and not last arg */
+			if (separator && i + 1 != n)
+				printf("%s", separator);
+		i++;
+	}
 	/* free list mem */
 	va_end(args);
 
