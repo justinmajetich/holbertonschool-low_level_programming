d825bd484a91d1e6943d5b6c32c1f8309c69e19c
justinmajetich <justinmajetich@gmail.com>
2019-10-27 19:37:30 +0000

    Change _start to main

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,21 +1,18 @@
-global _start
-
-%define sys_write 1
-%define sys_exit 60
-
-section	.data
-	hello	db	"Hello, Holberton", 10
-	len	equ	$-hello
-
 section	.text
 
-_start:
-	mov rax, sys_write
+global main
+
+main:
+	mov rax, 1
 	mov rdi, 1
 	mov rsi, hello
 	mov rdx, len
 	syscall
 
-	mov rax, sys_exit
+	mov rax, 60
 	mov rdi, 0
 	syscall
+
+section	.rodata
+	hello:	db	"Hello, Holberton", 10
+	len:	equ	$ - hello
