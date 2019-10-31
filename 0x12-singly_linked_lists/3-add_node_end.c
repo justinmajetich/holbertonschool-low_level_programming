2f41015da5c2fb6731c03fccf6477428c0117a7c
justinmajetich <justinmajetich@gmail.com>
2019-10-31 23:19:04 +0000

    Write script to append new end node to list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+int _strlen(const char *s);
+/**
+ * add_node_end - append node to end of list
+ * @head: pointer to list head
+ * @str: value to copy to new node
+ *
+ * Return: pointer to new end node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head; /* stores head address for iteration */
+	list_t node;
+
+	/* assign pointer to new node */
+	new = &node;
+
+	/* allocate for new node */
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	/* allocate for string member */
+	new->str = malloc(sizeof(char) * (_strlen(str)));
+	if (!new->str)
+		return (NULL);
+
+	/* initialize new node */
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = NULL;
+
+	/* if list is null, assign new node as head */
+	if (!temp)
+		*head = new;
+	else
+	{
+		/* find end of head list */
+		while (temp->next)
+			temp = temp->next;
+		/* point current end node to new end node */
+		temp->next = new;
+	}
+
+	return (new);
+}
+/**
+* _strlen - Function that finds the length of a string
+* @s: string that the length of is found
+*
+* Return: length of the @s
+*/
+int _strlen(const char *s)
+{
+	/* variable to loop through string to get length */
+	int len = 0;
+
+	/* Loop through string, checking for null byte */
+	while (s[len] != '\0')
+		len++;
+
+	/* return length variable once null byte is reached */
+	return (len);
+}
