41c09125aa2d5666f54ecccc7f43e10f9f93a303
justinmajetich <justinmajetich@gmail.com>
2019-11-01 02:24:00 +0000

    Print text before main executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void pre_main(void) __attribute__ ((constructor));
+/**
+ * pre_main - print text
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}
