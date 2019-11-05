cd855a4b6f36f913df93b5287a668f7b3f7ee05a
justinmajetich <justinmajetich@gmail.com>
2019-11-05 19:26:12 +0000

    Write script to reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse a list
+ * @head: list to reverse
+ *
+ * Return: pointer to head of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current;
+	listint_t *tail;
+
+	/* if list doesn't exist, return NULL */
+	if (!(*head))
+		return (NULL);
+
+	/* inititalized pointers to starting positions */
+	tail = current = *head;
+	*head = (*head)->next;
+	tail->next = NULL;
+
+	/* iterate to end of list, repointing nexts */
+	while ((*head)->next)
+	{
+		current = *head;
+		*head = (*head)->next;
+		current->next = tail;
+		tail = current;
+	}
+
+	/* reverse end node pointer */
+	(*head)->next = tail;
+
+	return (*head);
+}
