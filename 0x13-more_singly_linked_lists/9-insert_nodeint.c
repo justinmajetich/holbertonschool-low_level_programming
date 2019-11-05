91aba236020c75f90722c9b828f50c66448ccdd3
justinmajetich <justinmajetich@gmail.com>
2019-11-05 18:22:08 +0000

    Add call to add_nodeint if idx is zero

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+listint_t *add_nodeint(listint_t **head, const int n);
 /**
  * insert_nodeint_at_index - insert new node at nth position
  * @head: list to add to
@@ -21,7 +22,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/* initialize temp pointer to value of head */
 	temp = *head;
 
-	/* navigate to index */
+	/* if index is zero */
+	if (!idx)
+		return (add_nodeint(head, n));
+
+	/* if idx is not zero, navigate to index */
 	while (i < idx && temp)
 	{
 		temp = temp->next;
@@ -46,6 +51,36 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 		return (NULL);
+
+	return (new);
+}
+/**
+ * add_nodeint - add new node to beginning of list
+ * @head: pointer to list head
+ * @n: value to initialize in new node
+ *
+ * Return: address of new node on Success, NULL on Fail
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t node; /* new node */
+	listint_t *new = &node; /* pointer to new node */
+
+	/* check for NULL head */
+	if (!head)
+		return (NULL);
+
+	/* allocate for new node */
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	/* initialize new node */
+	new->n = n;
+	new->next = *head; /* point new head to old head */
+
+	/* assign head pointer to new head */
+	*head = new;
 
 	return (new);
 }
