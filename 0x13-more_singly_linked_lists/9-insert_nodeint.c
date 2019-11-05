30beafb28494d85050002752476ba9a42089dcdc
justinmajetich <justinmajetich@gmail.com>
2019-11-05 18:08:19 +0000

    Check if double pointer is  NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 1;
 
 	/* return NULL, if empty list */
-	if (!(*head))
+	if (!head)
 		return (NULL);
 
 	/* initialize temp pointer to value of head */
