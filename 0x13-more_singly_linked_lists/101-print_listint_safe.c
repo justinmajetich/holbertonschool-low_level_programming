f37cddd115eff185c3b3ef849033a9ea5a13b423
justinmajetich <justinmajetich@gmail.com>
2019-11-06 22:23:20 +0000

    Add ability to determine loop entry

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+const listint_t *find_loop_entry(const listint_t *head);
+/**
+ * print_listint_safe - print a looping list
+ * @head: list head
+ *
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t node_cnt = 0;
+	int entered = 0; /* flags loop entry */
+	const listint_t *temp, *entry;
+
+	/* exit if list is empty */
+	if (!head)
+		exit(98);
+
+	temp = head;
+	entry = find_loop_entry(head);
+
+	/* loop until back to head */
+	do {
+		if (temp == entry)
+		{
+			if (entered)
+				break;
+			else
+				entered++;
+		}
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		node_cnt++;
+		temp = temp->next;
+	} while (temp);
+
+	return (node_cnt);
+}
+/**
+ * find_listint_loop - find loop entry in list
+ * @head: start of list
+ *
+ * Return: address of loop entry on Success, NULL on Fail
+ */
+const listint_t *find_loop_entry(const listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	for (; head; head = head->next)
+	{
+		for (slow = head->next, fast = head->next->next; slow && fast;)
+		{
+			if (slow == head || fast == head)
+				return (head);
+			
+			if (slow == fast)
+				break;
+
+			if (!slow->next || !fast->next->next)
+				return (NULL);
+
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+	}
+	return (NULL);
+}
