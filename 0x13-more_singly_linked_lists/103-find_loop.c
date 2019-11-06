674159a62576b79166eb76a7a1762b1db550bd1f
justinmajetich <justinmajetich@gmail.com>
2019-11-06 21:46:26 +0000

    Write script to find loop in list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find loop entry in list
+ * @head: start of list
+ *
+ * Return: address of loop entry on Success, NULL on Fail
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	for (; head; head = head->next)
+	{
+		for (slow = head->next, fast = head->next->next; slow && fast;)
+		{
+			if (slow == head || fast == head)
+				return (head);
+			
+			if (slow == fast)
+				break;
+
+			if (!slow->next || !fast->next->next)
+				return (NULL);
+
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+	}
+	return (NULL);
+}
