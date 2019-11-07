1977df5f0c7d566731f6a502ab60ae063a8c078f
justinmajetich <justinmajetich@gmail.com>
2019-11-07 21:18:52 +0000

    Return 1 if bit already cleared

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,6 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	test >>= index;
+
 	if (test & 1)
 	{
 		/* set mask */
@@ -24,5 +25,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (1);
 	}
 	else
-		return (-1);
+		return (1);
 }
