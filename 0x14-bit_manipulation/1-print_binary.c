11e52dd7f03743f8bde2d4c5cbd5f5976b5528a3
justinmajetich <justinmajetich@gmail.com>
2019-11-07 19:20:01 +0000

    Re-write function to be recursive

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,20 +5,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, digit, flag = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
+	if (n & 1)
+		_putchar('1');
+	else
 		_putchar('0');
-
-	for (i = 31; i >= 0; i--)
-	{
-		digit = n >> i;
-		if (digit & 1)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		if (!(digit & 1) && flag)
-			_putchar('0');
-	}
 }
