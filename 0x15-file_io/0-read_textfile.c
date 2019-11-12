1657934ce1b51b26d7261b11c45ed68569448635
justinmajetich <justinmajetich@gmail.com>
2019-11-12 19:12:11 +0000

    Rearrange malloc statement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 
 /**
  * read_textfile - reads and prints text to stdout
+ * @filename: file to read/print from
  * @letters: number of bytes to read/print
  *
  * Return: number of bytes read/printed, 0 if unreadable or !filename
@@ -9,28 +10,32 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, print_cnt;
-	char *buffer = malloc(sizeof(char) * letters);
+	char *buffer;
 
-	/* check for NULL arg and malloc fail */
-	if (!filename || !buffer)
+	/* check for NULL arg */
+	if (!filename)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 		return (0);
 
 	/* retrieve file descriptor for opened file */
 	fd = open(filename, O_RDONLY);
-	if (fd < 0) /* check if open success */
+	if (fd == -1) /* check if open success */
 		return (0);
-	
+
 	/* read file to buffer, check success */
-	if(read(fd, buffer, letters) < 0)
+	if (read(fd, buffer, letters) == -1)
 		return (0);
 
 	/* write buffer to stdout, check success */
 	print_cnt = write(STDOUT_FILENO, buffer, letters);
-	if (print_cnt < 0)
+	if (print_cnt == -1)
 		return (0);
 
 	/* close file descript, check success */
-	if (close(fd) < 0)
+	if (close(fd) == -1)
 		return (0);
 
 	/* free memory allocated to buffer */
