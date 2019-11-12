8bd655d9b5bb2efe9261cac4b758ed9c1587c9ee
justinmajetich <justinmajetich@gmail.com>
2019-11-12 20:27:49 +0000

    Create script to append file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * append_text_to_file - append text to end of file
+ * @filename: file to append
+ * @text_content: text to append to file
+ *
+ * Return: 1 on Success, -1 on Failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	/* guard against NULL filename arg */
+	if (!filename)
+		return (-1);
+
+	/* open file with write and append flags */
+	fd = open(filename, (O_WRONLY | O_APPEND));
+	if (fd == -1) /* check for failed open */
+		return (-1);
+
+	if (text_content) /* append file, checking for failure */
+		if (write(fd, text_content, _strlen(text_content) == -1))
+			return (-1);
+
+	/* close file descriptor */
+	close(fd);
+
+	return (1);
+}
+/**
+* _strlen - Function that finds the length of a string
+* @s: string that the length of is found
+*
+* Return: length of the @s
+*/
+int _strlen(char *s)
+{
+	/* variable to loop through string to get length */
+	int len = 0;
+
+	/* Loop through string, checking for null byte */
+	while (s[len] != '\0')
+		len++;
+
+	/* return length variable once null byte is reached */
+	return (len);
+}
