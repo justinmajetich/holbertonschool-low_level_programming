119534ecc88c368fbd6e0725a9d30d8a7817a47a
justinmajetich <justinmajetich@gmail.com>
2019-11-13 19:12:09 +0000

    Comment out close in open fail

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,12 +28,12 @@ int main(int argc, char **argv)
 	f2 = open(argv[2], (O_WRONLY | O_TRUNC | O_CREAT), 00664);
 	if (f2 == -1)
 	{
-		close_check = close(f1);
+		/*close_check = close(f1);
 		if (close_check == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 			exit(100);
-		}
+		}*/
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
