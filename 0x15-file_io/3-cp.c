401bafb9151c4b6bb5063bd38804b006319ecf93
justinmajetich <justinmajetich@gmail.com>
2019-11-13 04:00:24 +0000

    Switch up open fail if

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,13 +28,13 @@ int main(int argc, char **argv)
 	f2 = open(argv[2], (O_WRONLY | O_TRUNC | O_CREAT), 00664);
 	if (f2 == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close_check = close(f1);
 		if (close_check == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 			exit(100);
 		}
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	/* copy contents from old file to new */
