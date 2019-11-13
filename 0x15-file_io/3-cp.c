1550a29f2ae5c5a0409e8f825db02d5fc4a06ba7
justinmajetich <justinmajetich@gmail.com>
2019-11-13 03:30:48 +0000

    Guard against bad close

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,11 @@ int main(int argc, char **argv)
 	if (f2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(f1);
+		if ((close(f1)) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+			exit(100);
+		}
 		exit(99);
 	}
 	/* copy contents from old file to new */
