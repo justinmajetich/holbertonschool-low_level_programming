78268204d6a2c6e21a3fe9d890d8d4cc4780255a
justinmajetich <justinmajetich@gmail.com>
2020-01-18 17:03:56 +0000

    Write function to set hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add an element to hash table
+ * @ht: hash table to add to
+ * @key: key of new element
+ * @value: value of new element
+ *
+ * Return: 1 on Success; 0 on Failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL;
+
+	/* check for NULL/empty key */
+	if (!ht || !key || (strlen(key) == 0) || !value)
+		return (0);
+
+	/* derive index from key */
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+
+	/* create new node */
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	/* initialize node with value duplicate */
+	new->key = strdup((char *)key); /* store key in node */
+	new->value = strdup(value); /* dup value to node */
+
+	new->next = ht->array[index]; /* attach new to head of list at index */
+	ht->array[index] = new; /* assign index pointer to new node */
+
+	return (1);
+}
